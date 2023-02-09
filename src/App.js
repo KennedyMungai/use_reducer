19339ceb70230dfad2f9e1c6d0928af2ae9b313d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-09 12:22:35 +0300

    Deleted the AuthContext import statement

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
 
 function App()
 {
