e567e7aa9cdb0ab346b36253d465e4e461c93161
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-09 12:08:02 +0300

    Spat out the jsx code from the return statement

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -7,26 +7,30 @@ const Navigation = (props) =>
 {
   return (
     <AuthContext.Consumer>
-      {(ctx) => { }}
-      <nav className={classes.nav}>
-        <ul>
-          {props.isLoggedIn && (
-            <li>
-              <a href="/">Users</a>
-            </li>
-          )}
-          {props.isLoggedIn && (
-            <li>
-              <a href="/">Admin</a>
-            </li>
-          )}
-          {props.isLoggedIn && (
-            <li>
-              <button onClick={props.onLogout}>Logout</button>
-            </li>
-          )}
-        </ul>
-      </nav>
+      {(ctx) =>
+      {
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {props.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {props.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {props.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        )
+      }}
     </AuthContext.Consumer>
   );
 };
