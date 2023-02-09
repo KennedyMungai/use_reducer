f91df098b0a736a78fe73f26028884128218108f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-09 11:54:04 +0300

    Added a button component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Button from '../UI/Button/Button';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
-const Home = (props) => {
+const Home = (props) =>
+{
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onclick={props.onLogout}>Logout</Button>
     </Card>
   );
 };
