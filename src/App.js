8ae94ee2733915f1c8e615022b4d443eb1ae96f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-09 13:10:52 +0300

    Removed the AuthContext import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
 
 function App()
 {
