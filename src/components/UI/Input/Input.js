0d02adc058991db27919cdfb8ac133916e938c9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-10 08:19:04 +0300

    Refactored the component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,16 +6,16 @@ const Input = (props) =>
 {
     return (
         <div
-            className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
+            className={`${classes.control} ${props.isValid === false ? classes.invalid : ''
                 }`}
         >
-            <label htmlFor="email">E-Mail</label>
+            <label htmlFor={props.id}>{props.label}</label>
             <input
-                type="email"
-                id="email"
-                value={emailState.value}
-                onChange={emailChangeHandler}
-                onBlur={validateEmailHandler}
+                type={props.type}
+                id={props.id}
+                value={props.value}
+                onChange={props.onChange}
+                onBlur={props.onBlur}
             />
         </div>
     )
